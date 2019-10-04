35795895a1294ffde9e9bc3fa5a8483ef0573f25
Damir Pasagic <damir.pasagic@leica-geosystems.com>
2019-10-04 16:06:38 +0200

    Initial commit

---
+++ Test-VS-GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_VS_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
